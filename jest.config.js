46b5a4ac0ca36e255e26c9d0172778ba3636acb7
Breno Sonda <breno26sonda@gmail.com>
2023-04-04 17:02:48 -0300

    added coverage report

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,13 @@ module.exports = {
     moduleNameMapper: {
         "\\.(scss|css|sass)$": "identity-obj-proxy",
         "\\.svg": "<rootDir>/tests/mocks/svgrMock.ts"
-    }
+    },
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "src/**/*.tsx",
+        "!src/**/*.spec.tsx",
+        "!src/**/*_app.tsx",
+        "!src/**/*_document.tsx"
+    ],
+    coverageReporters: ["lcov", "json"]
 }
